93e05cae39256afebc4925afe1d35733783d8b7f
darianeagu <120371196+darianeagu@users.noreply.github.com>
2023-04-23 10:01:29 +0000

    Create Authenticator class and update Customer

--- src/main/java/bank/Customer.java
+++ src/main/java/bank/Customer.java
@@ -7,6 +7,7 @@ public class Customer {
   private String username;
   private String password;
   private int accountId;
+  private boolean authenticated;
 
   //make the Constructor
   public Customer (int id, String name, String username, String password, int accountId) {
@@ -15,6 +16,7 @@ public class Customer {
     setUsername(username);
     setPassword(password);
     setAccountId(accountId);
+    setAuthenticated(false);
   }
 
   public int getId() {
@@ -55,6 +57,14 @@ public class Customer {
 
   public void setAccountId(int accountId) {
     this.accountId = accountId;
+  }
+
+  public boolean isAuthenticated() {
+    return this.authenticated;
+  }
+
+  public void setAuthenticated(boolean authenticated) {
+    this.authenticated = authenticated;
   }
 
 }
