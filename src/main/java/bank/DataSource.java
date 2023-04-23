6222216a552c2c26f2a731e5dc3c311d5c9bd7fa
darianeagu <120371196+darianeagu@users.noreply.github.com>
2023-04-23 09:04:38 +0000

    Create Account from data source

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -47,11 +47,34 @@ public class DataSource {
 
   }
 
+  public static Account getAccount(int accountId) {
+    String sql = "SELECT * FROM accounts WHERE id = ?"; 
+    Account account = null; 
+
+    try(Connection connection = connect();
+      PreparedStatement statement = connection.prepareStatement(sql)){
+        
+        statement.setInt(1, accountId);
+
+        try(ResultSet resultSet = statement.executeQuery()){
+          account = new Account(
+            resultSet.getInt("id"),
+            resultSet.getString("type"),
+            resultSet.getDouble("balance"));
+        }
+      }catch(SQLException e) {
+        e.printStackTrace();
+      }
+
+      return account;
+
+  }
+
   // Testing connection
 
   public static void main(String[] args) {
     Customer customer = getCustomer("twest8o@friendfeed.com");
-    System.out.println(customer.getName());
+    Account account = getAccount(customer.getAccountId());
+    System.out.println(account.getBalance());
   }
-
 }
