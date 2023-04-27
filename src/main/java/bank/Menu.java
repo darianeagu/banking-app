316c5d09c66baa617bdb12f2c217d2d754b66977
darianeagu <120371196+darianeagu@users.noreply.github.com>
2023-04-27 14:07:50 +0000

    Display menu options for user interaction

--- src/main/java/bank/Menu.java
+++ src/main/java/bank/Menu.java
@@ -22,7 +22,7 @@ public class Menu {
 
   }
 
-  private Customer authenticateUser(){
+  private Customer authenticateUser() {
     System.out.println("Please enter your username");
     String username = scanner.next();
 
@@ -30,18 +30,59 @@ public class Menu {
     String password = scanner.next();
 
     Customer customer = null;
-    
+
     try {
-      customer = Authenticator.login(username, password); 
+      customer = Authenticator.login(username, password);
     } catch (LoginException e) {
-      System.out.println("There was an error" + e.getMessage()); 
+      System.out.println("There was an error" + e.getMessage());
     }
-    
+
     return customer;
 
   }
 
   private void showMenu(Customer customer, Account account) {
+    int selection = 0;
+    while(selection != 4 && customer.isAuthenticated()) {
+      System.out.println("======================================");
+      System.out.println("Please select one of the following options: ");
+      System.out.println("1: Deposit");
+      System.out.println("2: Withdraw");
+      System.out.println("3: Check balance");
+      System.out.println("4: Exit");
+      System.out.println("======================================");
+
+      selection = scanner.nextInt();
+      double amount = 0;
+
+      switch(selection) {
+        case 1:
+        System.out.println("How much would you like to deposit?")
+        amount = scanner.nextDouble();
+        account.deposit(amount);
+        break;
+
+        case 2:
+        System.out.println("How much would you like to withdraw?")
+        amount = scanner.nextDouble();
+        account.withdraw(amount);
+        break;
+
+        case 3:
+        System.out.println("Current balance:" + account.getBalance());
+        break;
+
+        case 4:
+        Authenticator.logout(customer);
+        System.out.println("Thanks for banking at Globe Bank International");
+        break;
+
+        default:
+        System.out.println("Invalid option. Please try again");
+        break;
+      }
+
+    }
 
   }
 
